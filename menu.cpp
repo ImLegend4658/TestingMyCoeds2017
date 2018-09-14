fe6a3a04246b274097b415ecdf4837c2a5883ed6
MrAziz2009 <Cool_zizo2009@hotmail.com>
2018-09-14 19:56:15 -0400

    Add files via upload

---
+++ menu.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std; 
+
+/*
+Author Aziz Aldawk 
+
+This is for our project and my mission is doing menu function.
+shll only funciton on main to make it easy.....
+
+Note: maybe i need to gethering all program on once. 
+Note: This program need to be done on the weekend. 
+
+*/
+void Menu(); 
+int main(){
+
+//shll only function here. 
+
+Menu(); 
+// Great, my function is working and Shll i start so. :) 
+// make sure is looping until select (0); 
+
+}
+// last adding: 11/9/2018 
+void Menu(){
+
+//shll i write my code here later :D. << happy face @_@
+
+
+
+}
