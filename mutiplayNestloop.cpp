60e2c70b29bff2d18893882e405322f8cf6e30c3
ImLegend4658 <MrAziz2005@gmail.com>
2020-08-01 01:17:58 +0300

    Add files via upload

---
+++ mutiplayNestloop.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<string>
+ using namespace std; 
+
+
+int main(){
+/*
+This program will multiplay 1 to 10 by two loops only 
+it called nested loops.
+all copyright Aziz@2020
+*/
+int sum=0;
+
+for (int i = 1; i < 11; i++)
+{
+for (int j = 0; j < 11; j++)
+{
+	
+	cout<<i<<" * "<<j<<" = "<<i*j<<endl;
+	
+}
+ cout<<"=========="<<endl;
+}
+
+ }
+
+ 
+
+ 
