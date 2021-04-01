ca0d439600c4fe1e27f0ac1d9a394cc40355ceda
ImLegend4658 <MrAziz2005@gmail.com>
2021-04-01 09:31:26 +0300

    Add files via upload

---
+++ Generete_insert.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+ using namespace std; 
+/*
+Author: Aziz Aldawk 
+
+This program will generate many insert instead of menual. 
+so, this is only for pagination or testing program. 
+feel free to develop it on your own. :)
+*/
+
+int main(){
+
+
+for (int i =1;i<20;i++){
+
+	cout<<"Insert into customer values("<<i<<",'Aziz','AA',4323);"<<endl;
+}
+
+ }
