0f61592bbc1abc3abeaef4f0621d23868d3178c1
ImLegend4658 <MrAziz2005@gmail.com>
2021-05-05 20:51:29 +0300

    Add files via upload

--- vLet_resv.cpp
+++ vLet_resv.cpp
@@ -1,16 +1,40 @@
 #include<iostream>
 #include<string>
  using namespace std; 
+/*
+Author: Abdulaziz Aldawk
+Date: 5/5/2021
 
+All copyright @Abdulaziz Aldawk..
+
+feel free to study my code but 
+please dont copy it for your homework and do it differently
+or in your way. 
+
+*/
 struct num{
    string name;
 };
+
+// structure to store name as string type. 
 
 void entername(num &thisname);
 void countChar(num &thisname);
 void backWord(num &thisname);
 void vo_letter(num &thisname);
+/*
+Those are functions:
 
+entername: functions prompt to user to enter name 
+and user will enter name. 
+his name will store at struct name type. 
+
+countchar: it will count how many character of name without space.
+
+backWord: it will show his name as backword. 
+
+Vo_letter: it will count how many vowes in the name.
+*/
 int main(){
 num thisname; 
 
@@ -18,7 +42,7 @@ entername(thisname);
 countChar(thisname);
 backWord(thisname);
 vo_letter(thisname);
-
+//function call.
 
 }
 
@@ -26,8 +50,9 @@ vo_letter(thisname);
 void entername(num &thisname){
    cout<<"Enter your name sir:"<<endl; 
    getline(cin,thisname.name);
-
+//prompt to user to write his name.
  cout<<"Your name: "<<thisname.name<<endl;
+// prompt user name. 
 }
 
 
@@ -43,7 +68,9 @@ void countChar(num &thisname){
 
       } 
    }
+// for loop to count the character without space.
    cout<<"Letters: "<<len-1<<endl;
+//prompt string without space
    
 }
 
@@ -55,6 +82,8 @@ temp+= thisname.name[i];
 }
 cout<<temp<<endl;
 }
+//backword string or character
+
 
 void vo_letter(num &thisname){
    int a =0;
@@ -88,3 +117,37 @@ void vo_letter(num &thisname){
    cout<<"U: "<<U<<endl;
 
 }
+
+the output:
+Enter your name sir:
+Aziz Aldawk
+Your name: Aziz Aldawk
+Letters: 10
+kwadlA zizA
+A: 3
+E: 0
+I: 1
+O: 0
+U: 0
+
+Enter your name sir:
+Khalid alshamary  
+Your name: Khalid alshamary
+Letters: 15
+yramahsla dilahK
+A: 4
+E: 0
+I: 1
+O: 0
+U: 0
+
+Enter your name sir:
+Saad Alotaibi
+Your name: Saad Alotaibi
+Letters: 12
+ibiatolA daaS
+A: 4
+E: 0
+I: 2
+O: 1
+U: 0
